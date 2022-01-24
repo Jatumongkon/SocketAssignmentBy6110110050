ef2257158bda04349379c55d4d5c50b644aa27b8
Jatumongkhon Saengphum <6110110050@psu.ac.th>
2022-01-24 22:07:28 +0700

    key

--- client.js
+++ client.js
@@ -2,38 +2,51 @@ var net = require('net');
 
 var HOST = '127.0.0.1';
 var PORT = 6969;
+var my_key = 'aaa'
 
-
-let num = 20
-
+let num = 0
+let state = 0 //idle 
 var client = new net.Socket();
 
 client.connect(PORT, HOST, function() {
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     client.write('start');
-
     
 });
     client.on('data', function(data) {
         console.log('รับ :' + data);
-        if (data == 'start!'){
-            console.log('ส่ง : ' + num);
-            client.write(`${num}`);}
-        if (data != 'start!'){
-            num = parseInt(data) + 3
-            console.log('ส่ง : ' + num);
-            client.write(`${num}`);}
-        if (data == 'BYE'){
-            console.log('รับ : ' + data);
-            client.destroy();
+        if (data == 'start!' && state ==0){
+            console.log('ส่ง key : ' + my_key);
+            client.write(`${my_key}`);
+            state = 1
         }
-        if(data == 'INVALID'){
+        if (data != 'start!' && state == 1){
+            num = parseInt(data) + 3
+
+            if(data != NaN)
+                console.log('ส่ง : ' + num);
+            client.write(`${num}`);
+
+        }
+        if (data == 'BYE' && state == 1){
+            client.destroy();
+            console.log('รับ : ' + data);
+            state = 2 // End
+        }
+        if(data == 'INVALID'&& state == 1){
             console.log('INVALID');
             client.destroy();
+            state = 2 // End
         }
-        if (data == 'NaN'){
+        if (data == 'NaN' && state == 1){
             console.log('Server turn off ');
             client.destroy();
+            state = 2 // End
+        }       
+        if (data == 'NotKey' && state == 1){
+            console.log('Sorry !!');
+            client.destroy();
+            state = 2 // End
         }
     });
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 6969;
-
+var key_list = ['aaa','bbb']
+var is_client_key = false
 
 
 net.createServer(function (sock) {
@@ -13,18 +14,35 @@ net.createServer(function (sock) {
             sock.write('start!') //
             state = 1 //wait data
             }
+        if(state == 1){
+            for (let i = 0 ;i < key_list.length ; i+=1){
+                if(key_list[i] == data){
+                    console.log('รับ key : ' + data);
+                    sock.write(`${0}`)
+                    is_client_key = true
+                    state = 2 
+                }
+                
+            }
+            
+        }
         
-        if(parseInt(data) < 10 &&state ==1){
+        if(parseInt(data) < 10 &&state ==2){
             console.log('รับ ' +`${data*2}`)
             sock.write(`${data*2}`)                     
         }
-        else if(parseInt(data) > 10 &&state ==1){
+        else if(parseInt(data) > 10 &&state ==2 && data != 'BYE'){
             console.log('BYE')
             sock.write('BYE');
         }
-        else if(data != 'start' &&state ==1 && data != 'NaN'){
+        else if(data != 'start' &&state ==2 && data != 'NaN' && !is_client_key){
             console.log('INVALID');
             sock.write('INVALID');
+        }
+        else if(!is_client_key && state != 1){
+            console.log('Key not in list ');
+            sock.write('NotKey');
+            state = 3 //End
         }
         else if (data == 'NaN'){
             console.log('Client turn off ');
