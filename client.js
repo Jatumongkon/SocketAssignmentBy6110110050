2894a2b4eb86d30a05474663beb5f57c3b49999e
Jatumongkhon Saengphum <6110110050@psu.ac.th>
2022-01-24 21:23:36 +0700

    add state

--- client.js
+++ client.js
@@ -1,18 +1,17 @@
-
-
 var net = require('net');
 
 var HOST = '127.0.0.1';
 var PORT = 6969;
 
 
-let num = 0
+let num = 20
 
 var client = new net.Socket();
 
 client.connect(PORT, HOST, function() {
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     client.write('start');
+
     
 });
     client.on('data', function(data) {
@@ -28,6 +27,14 @@ client.connect(PORT, HOST, function() {
             console.log('รับ : ' + data);
             client.destroy();
         }
+        if(data == 'INVALID'){
+            console.log('INVALID');
+            client.destroy();
+        }
+        if (data == 'NaN'){
+            console.log('Server turn off ');
+            client.destroy();
+        }
     });
 
 
@@ -35,16 +42,3 @@ client.connect(PORT, HOST, function() {
         console.log('Connection closed');
         client.destroy();
     });
-
-
-
-//     client.on('data', function(data) {
-//         console.log('รับ : ' + data);
-//         if (data == 'start!')
-//             console.log('ส่ง: ' + num);
-//             client.write(`${num}`);
-//         if (data != 'start!'){
-//             console.log('ส่ง: ' + num);
-//             num = data + 3
-//             client.write(`${num}`);}
-//    ;});
--- server.js
+++ server.js
@@ -6,19 +6,29 @@ var PORT = 6969;
 
 net.createServer(function (sock) {
     var state = 0 //idle 
+    
     sock.on('data', function (data) {
-    console.log('รับ: ' + data);
-        if(data == 'start'){
-            sock.write('start!')
+        if(data == 'start' && state == 0){
+            console.log('รับ: ' + data);
+            sock.write('start!') //
+            state = 1 //wait data
             }
         
-        if(parseInt(data) < 10){
+        if(parseInt(data) < 10 &&state ==1){
             console.log('รับ ' +`${data*2}`)
             sock.write(`${data*2}`)                     
         }
-        else if(parseInt(data) > 10){
+        else if(parseInt(data) > 10 &&state ==1){
             console.log('BYE')
-                        
+            sock.write('BYE');
+        }
+        else if(data != 'start' &&state ==1 && data != 'NaN'){
+            console.log('INVALID');
+            sock.write('INVALID');
+        }
+        else if (data == 'NaN'){
+            console.log('Client turn off ');
+            state = 3 //End
         }
     });
     
